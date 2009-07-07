264299100918859432c7fff04dd1449025a05010
T. Andrew Binkowski <tbinkowski@tbinkowski4.sbc.anl.gov>
2009-07-07 11:29:26 -0500

    Update ZINC_downloadAndFile.pl
     * Download both usual and reference files from server
     * Change name format ZINC######.0-1.mol2

--- ZINC_downloadAndFile.pl
+++ ZINC_downloadAndFile.pl
@@ -14,6 +14,10 @@
 #        possible library (e.g. all-purchasable) because it
 #        with overwrite duplicate files.  As long as they are
 #        run once they will be incremented accrodingly.
+#
+# Example: 
+#  awk '{print "perl ZINC_downloadAndFile.pl -id "$2" -zinc_library ../Zinc-Library/"}' Subsets/23_t70.smi
+#
 ##############################################################
 use File::Path;
 use File::Basename;
@@ -28,7 +32,8 @@ $|=1;
 # Command line arguments
 #############################################################
 my $DATE=`date +%Y%m%d`;chomp($DATE);
-my $USAGE="\n\t".basename($0)." -zinc_library [-id zinc_id]\n";
+my $USAGE="\n\t".basename($0)." -zinc_library /Volumes/Alpha/Zinc/Zinc-Library/ -id ZINC#######\n";
+
 GetOptions(
     "zinc_library=s" => \$ZINC_LIBRARY,
     "id=s"           => \$ID,
@@ -38,6 +43,8 @@ GetOptions(
     );
 die("\n$USAGE\n") if $HELP;
 die("$ZINC_LIBRARY doesn't exist") if !-e $ZINC_LIBRARY;
+
+print "ZINC MOLECULE: $ID\n";
 
 #############################################################
 # Set a description string
@@ -49,8 +56,6 @@ my $count=0;
 # line of the last molecule print
 #
 #############################################################
-# List mode
-#open(MOL2_FILE,"<$MOL2_FILE");
 
 #############################################################
 # Set the path
@@ -59,7 +64,7 @@ $subdir=substr($ID,4,2);
 $subsubdir=substr($ID,6,2);
 $subsubsubdir=substr($ID,8,2);
 $fullPath = "$ZINC_LIBRARY/$subdir/$subsubdir/$subsubsubdir/";
-mkpath($fullPath);
+print "Path: $fullPath\n";
 
 #############################################################
 # Quick Test and Force
@@ -77,41 +82,58 @@ if(scalar @molecules>0) {
 }
 
 #############################################################
-# Download from zinc
+# Download both the level 0 (reference) and level 1 (usual)
+# from the zinc servers
 #############################################################
-$mol2_string = `curl --silent "http://zinc8.docking.org/fget2.pl?f=m&l=1&z=$ID"`;
-@mol2_array = split(/\n/,$mol2_string);
-if (scalar @mol2_array==0) {
-    die("Problem downloading from Zinc.");
-}
+for $level (0..1) {
+    print "Looking for level:$level\n";
 
-#############################################################
-# Process the download and place into directory
-#############################################################
-foreach(@mol2_array) {
-    $line="$_\n";
-    #print $line;
-    $finishLastMolecule = ($lineCount==$#mol2_array) ? 1 : 0;
+    # Reset the counter variables
+    @mol2_array=();
+    $lineCount=0;
+    $flag=0;
+    %idHash={};
 
-    if($line=~/(ZINC\d+)/) {
-	$id=$1;
-	$returnString.="@<TRIPOS>MOLECULE\n";
-	$flag=1;
+    # Download from ZINC
+    $mol2_string = `curl --silent "http://zinc8.docking.org/fget2.pl?f=m&l=$level&z=$ID"`;
+    @mol2_array = split(/\n/,$mol2_string);
+
+    # Test if Zinc returned anything
+    if (scalar @mol2_array==0) {
+	print "Didn't find any level: $level.\n";
+    } else {
+	mkpath($fullPath);
+
+	####
+	# Create a unique file for each ZINC molecule
+	####
+	foreach(@mol2_array) {
+	    $line="$_\n";
+	    print "$lineCount - $line";
+	    $finishLastMolecule = ($lineCount==$#mol2_array) ? 1 : 0;
+
+	    # Find the first line
+	    if($line=~/(ZINC\d+)/) {
+		$id=$1;
+		$returnString.="@<TRIPOS>MOLECULE\n";
+		$flag=1;
+	    }
+	    if (($line=~/@<TRIPOS>MOLECULE/ || $finishLastMolecule) && $flag==1) {	
+		++$idHash{$id};
+		$currentFile = "$fullPath/$id.$level-".$idHash{$id}.".mol2";
+		print "Writing: $currentFile\n";
+		open(OUT,">$currentFile") or die("Couldn't open $currentFile");
+		print OUT $returnString;
+		print OUT $line if $finishLastMolecule;
+		close OUT;
+		$flag=0;
+		$returnString="";
+	    }
+	    
+	    if($flag==1) {
+		$returnString.=$line;
+	    }
+	    $lineCount++;
+	}
     }
-    if (($line=~/@<TRIPOS>MOLECULE/ || $finishLastMolecule) && $flag==1) {	
-	++$idHash{$id};
-	$currentFile = "$fullPath/$id.".$idHash{$id}.".mol2";
-	open(OUT,">$currentFile") or die("Couldn't open $currentFile");
-	print OUT $returnString;
-	print OUT $line if $finishLastMolecule;
-	close OUT;
-	$flag=0;
-	$returnString="";
-    }
-    
-    if($flag==1) {
-	$returnString.=$line;
-    }
-    $lineCount++;
 }
-    
