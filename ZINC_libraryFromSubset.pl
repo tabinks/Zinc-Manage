e1503eddbf07a0ecb038f20a6e3ad0d116d6fc36
T. Andrew Binkowski <tbinkowski@tbinkowski4.sbc.anl.gov>
2009-07-08 15:41:48 -0500

    Add ZINC_libraryFromSubset.pl script to create library

---
+++ ZINC_libraryFromSubset.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+#################################################################
+# 
+#
+# File should be in format: smiles id
+#
+#################################################################
+use File::Path;
+use File::Basename;
+use File::Spec;
+
+use Getopt::Long;
+
+my %options=();
+
+GetOptions("subset=s"  => \$SUBSET,
+	   "prefix=s"   => \$PREFIX,
+	   "h!"         => \$HELP,
+	   "library=s" =>\$ZINC_LIBRARY,
+	   "usual_only!" => \$USUAL_ONLY);
+
+my $SYSTEM = $ENV{'PS1'};
+$SYSTEM=~s/\\[a-z]|[:@\.>]//g;
+$SYSTEM=~s/\s+//g;
+my $USER  = `whoami`;chomp($USER);
+my $DATE  = `date +%Y%m%d`;chomp($DATE);
+#my $DESCRIPTION = $PREFIX."_".$PROTEIN."_".basename($LIBRARY);
+
+die("ZINC_LIBRARY not found") if !-e $ZINC_LIBRARY;
+
+################################################################
+#if($SYSTEM eq "surveyor") {
+
+my $DB_PATH = $PREFIX.".$DATE.db";
+my $MISSING_PATH = $PREFIX.".$DATE.missing";
+$ZINC_LIBRARY = File::Spec->rel2abs($ZINC_LIBRARY);
+
+
+
+open(OUT,">$DB_PATH") or die("Couldn't open $DB_PATH");
+open(MISSING,">$MISSING_PATH") or die("Couldn't open $MISSING_PATH");
+
+open(SUBSET,"<$SUBSET") or die("Couldn't open $SUBSET");
+while(<SUBSET>) {
+    chomp();
+    ($smiles,$id)=split;
+    $subdir=substr($id,4,2);
+    $subsubdir=substr($id,6,2);
+    $subsubsubdir=substr($id,8,2);
+    
+    foreach(glob("$ZINC_LIBRARY/$subdir/$subsubdir/$subsubsubdir/$id.*-*.mol2")) {
+	$_=~/$id.(\d)\-(\d).mol2/;
+	$level=$1;
+	$count=$2;
+	next if $USUAL_ONLY && $level==0;
+	print "$level $count - $_\n";;
+	if (-e $_) {
+	    print OUT "$_ $smiles\n";
+	} else {
+	    print MISSING "$_ $smiles\n";
+	}
+    }
+    print "." if ($count++%100==0);
+}
+
+close SUBSET;
+close MISSING;
+close OUT;
